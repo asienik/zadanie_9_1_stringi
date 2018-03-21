836d323e54818ff3d3edad8b0022f462ded41340
Joanna Fengler <asiapobuta@wp.pl>
2018-03-21 19:15:23 +0100

    task_tree

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+var n = 8;
+var	i;
+var j;
+function drawTree(n) {
+	var star;
+	for(i=0; i<n; i++) {
+		var star = "";
+		for(j=0; j<=i; j++) {
+			star += "*";	
+		} 
+		console.log(star);
+	}
+}
+drawTree(n);
